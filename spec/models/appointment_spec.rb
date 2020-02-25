e60690555049b8259a7e203ed4fca65eaf73f1f8
danielmitiku <wedanielmitiku@gmail.com>
2020-02-25 16:29:24 +0300

    added controllers

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Appointment, type: :model do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:counseling_id) }
+  it { should belong_to(:user) }
+  it { should belong_to(:counseling) }
 end
