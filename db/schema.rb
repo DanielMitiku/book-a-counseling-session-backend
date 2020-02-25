ddd46a91cf5d6283c82a5768f1f4f81062641ba4
danielmitiku <wedanielmitiku@gmail.com>
2020-02-25 13:57:22 +0300

    added appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_090315) do
+ActiveRecord::Schema.define(version: 2020_02_25_105645) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "counseling_id"
+    t.datetime "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "counselings", force: :cascade do |t|
     t.string "name"
