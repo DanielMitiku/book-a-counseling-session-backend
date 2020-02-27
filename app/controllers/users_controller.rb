5add13cfe959b0e1512da393affc888b3c0a557a
danielmitiku <wedanielmitiku@gmail.com>
2020-02-27 11:00:50 +0300

    added json_response and exception handler module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  # before_action :logged_in_user, only: [:show, :update, :destroy]
+  # before_action :correct_user,   only: [:update]
+  # before_action :admin_user,     only: [:destroy, :index]
+
+  def index
+    @user = User.all
+    
+  end
+
+  def show
+  end
+
+  def update
+    
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post  '/signup',  to: 'users#create'
+  # get    '/login',   to: 'sessions#new'
+  # post   '/login',   to: 'sessions#create'
+  # delete '/logout',  to: 'sessions#destroy'
+  resources :users
 end
