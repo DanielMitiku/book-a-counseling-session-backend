72d496a95b13b2aeab812e6b9766d06e5690a310
danielmitiku <wedanielmitiku@gmail.com>
2020-02-27 15:16:41 +0300

    added authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   post  '/signup',  to: 'users#create'
-  # get    '/login',   to: 'sessions#new'
-  # post   '/login',   to: 'sessions#create'
-  # delete '/logout',  to: 'sessions#destroy'
+  post 'auth/login', to: 'authentication#authenticate'
+
   resources :users do
     resources :appointments
   end
+  
   resources :counselings do
     resources :appointments
   end
