a343f113dd54a688fc5973b20c4741c6e5f61ba5
danielmitiku <wedanielmitiku@gmail.com>
2020-02-27 14:45:55 +0300

    added appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,41 @@
 class AppointmentsController < ApplicationController
+  before_action :set_user
+  before_action :set_user_appointment, only: [:show, :update, :destroy]
+
+  def index
+    json_response(@user.appointments)
+  end
+
+  def show
+    json_response(@appointment)
+  end
+
+  def create
+    @user.appointments.create!(appointment_params)
+    json_response(@appointment, :created)
+  end
+
+  def update
+    @appointment.update(appointment_params)
+    head :no_content
+  end
+
+  def destroy
+    @appointment.destroy
+    head :no_content
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:date, :counseling_id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_user_appointment
+    @appointment = @user.appointments.find_by!(id: params[:id]) if @user
+  end
 end
