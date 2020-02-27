aa9fa47bcfcfce36116a11aa229bd7c200d30d20
danielmitiku <wedanielmitiku@gmail.com>
2020-02-27 12:09:25 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   # get    '/login',   to: 'sessions#new'
   # post   '/login',   to: 'sessions#create'
   # delete '/logout',  to: 'sessions#destroy'
-  resources :users
-  resources :counselings
+  resources :users do
+    resources :appointments
+  end
+  resources :counselings do
+    resources :appointments
+  end
 end
