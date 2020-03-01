6c62311facea3a0c0385144cac8970eeac036743
danielmitiku <wedanielmitiku@gmail.com>
2020-03-01 23:15:09 +0300

    added is_privileged in jwt_encode paramter

--- app/auth/authenticate_user.rb
+++ app/auth/authenticate_user.rb
@@ -5,7 +5,7 @@ class AuthenticateUser
   end
 
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    JsonWebToken.encode({user_id: user.id, is_privileged: user.is_privileged}) if user
   end
 
   private
