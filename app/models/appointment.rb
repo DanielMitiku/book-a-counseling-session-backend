78ad96ca2e7f63453131d80f0904958eb20d656a
danielmitiku <wedanielmitiku@gmail.com>
2020-03-04 22:39:18 +0300

    updated user serializer

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,5 +5,15 @@ class Appointment < ApplicationRecord
 
   validates :user_id, presence: true
   validates :counseling_id, presence: true
+  validates :date, presence: true
+  validate :date_cannot_be_past
+
+  private
+	
+  def date_cannot_be_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can not be in the past.")
+    end		
+  end
 
 end
